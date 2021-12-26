f4b5d63e1a46c04f680cf2ac059f1d7b25ca89eb
Pedro Isaaco <Pedro.Isaaco@gmx.net>
2021-12-26 08:34:09 +0100

    randomizer included

--- model_creation.py
+++ model_creation.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 from datetime import datetime
 
 def print_current_time():
@@ -8,20 +10,27 @@ def print_current_time():
     current_time = now.strftime("%H:%M:%S")
     print("Current Time is ", current_time)
 
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+csv_data_file = './diceroll_reduced_new.csv'
+
 print_current_time()
-dices_data = pd.read_csv('./diceroll_slim_short.csv')
+dices_data = pd.read_csv(csv_data_file)
 print_current_time()
 print('file loaded.')
 dices_features = []
-for i in range(0, 25):
+for i in range(0, 10000):
     dices_features += [str(i)]
 X = dices_data[dices_features]
 y = dices_data.value
-dices_model = DecisionTreeRegressor(random_state=1)
-dices_model.fit(X, y)
 
-print("making predictions")
-print(X.head())
-print("the predictions are")
-print(dices_model.predict(X.head()))
-print('end.')
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
--- process_image.py
+++ process_image.py
@@ -1,20 +1,47 @@
+import os
 import glob
 import csv
+import numpy as np
 from bmp_image import BmpImage
 
+def include_column_titles(file, max):
+    numbers = [str(x) for x in range(0, max)]
+    numbers.insert(0, "value")
+    numbers_list = ",".join(numbers) + "\n"
+
+    with open(file, "r") as f:
+        contents = f.readlines()
+
+    contents.insert(0, numbers_list)
+
+    file_name, file_ext = os.path.splitext(file)
+    file_new = file_name + "_new" + file_ext
+    with open(file_new, "w") as f:
+        f.write("".join(contents))
+
+def create_random_dataset(max, bmp_dir, filename):
+    file_count = len(glob.glob(bmp_dir))
+    rnd_numbers = np.random.randint(0, file_count, max)
+    outfile = filename + "_rnd.csv"
+    with open(outfile, mode='w', newline='') as diceroll_file:
+        for file_index in rnd_numbers:
+            bmp_file = glob.glob(bmp_dir)[file_index]
+            print("writing file '%s' (at index %d) to file '%s'" %(bmp_file, file_index, outfile))
+            bmp_image_obj = BmpImage(bmp_file)
+            dices_writer = csv.writer(diceroll_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            dices_writer.writerow([bmp_image_obj.value] + bmp_image_obj.bmp_data)
+            length = bmp_image_obj.data_size
+    print("including column headers ...")
+    include_column_titles(outfile, length) #will be 120000
+
 bmp_dir = './Images/target_output/*.*'
-bmp_dir_slim = './Images/target_output/wuerfel_00036_b*.*'
-filename = 'diceroll.csv'
-filename_slim = 'diceroll_slim.csv'
-filename_slim = 'diceroll_slim_short.csv'
-i = 0
-with open(filename_slim, mode='w', newline='') as diceroll_file:
-    for bmp_file in glob.glob(bmp_dir):    
-        bmp_image_obj = BmpImage(bmp_file)
-        dices_writer = csv.writer(diceroll_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        dices_writer.writerow([bmp_image_obj.value] + bmp_image_obj.bmp_data)
-        i += 1
-        if i > 100:
-            exit()
+debug = True #switch here
+if (debug):
+    filename = 'diceroll_reduced'
+else:
+    filename = 'diceroll'
+
+include_column_titles("test.csv", 6)
+#create_random_dataset(10, bmp_dir, filename)
 #csv: https://realpython.com/python-csv/
 print('done.')
