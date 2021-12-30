7a473b470e2dc99ba63ac3dab23f23eab6e5d3ea
Pedro Isaaco <Pedro.Isaaco@gmx.net>
2021-12-30 16:04:10 +0100

    random dataset creation + debugging

--- model_creation.py
+++ model_creation.py
@@ -3,12 +3,12 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from datetime import datetime
+from process_image import create_random_dataset
 
-def print_current_time():
+def print_current_time(message):
     now = datetime.now()
-
     current_time = now.strftime("%H:%M:%S")
-    print("Current Time is ", current_time)
+    print("current time: %s - %s" %(current_time, message))
 
 def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
     model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
@@ -17,20 +17,30 @@ def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
     mae = mean_absolute_error(val_y, preds_val)
     return(mae)
 
-csv_data_file = './diceroll_reduced_new.csv'
-
-print_current_time()
+file_path = "./Images/target_output/*.*"
+file_name = "diceroll_reduced"
+#csv: https://realpython.com/python-csv/
+print_current_time("starting ...")
+#create data
+csv_data_file = create_random_dataset(100, file_path, file_name)
+print_current_time("data created.")
+#read data
 dices_data = pd.read_csv(csv_data_file)
-print_current_time()
-print('file loaded.')
+print_current_time("files loaded.")
+
+#process data
+max_columns = 10000
 dices_features = []
-for i in range(0, 10000):
+for i in range(0, max_columns - 1):
     dices_features += [str(i)]
 X = dices_data[dices_features]
 y = dices_data.value
+print_current_time("csv read.")
 
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
-
+print_current_time("model trained.")
 for max_leaf_nodes in [5, 50, 500, 5000]:
     my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
     print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+
+print_current_time("finished.")
--- process_image.py
+++ process_image.py
@@ -4,44 +4,28 @@ import csv
 import numpy as np
 from bmp_image import BmpImage
 
-def include_column_titles(file, max):
-    numbers = [str(x) for x in range(0, max)]
-    numbers.insert(0, "value")
-    numbers_list = ",".join(numbers) + "\n"
-
-    with open(file, "r") as f:
-        contents = f.readlines()
-
-    contents.insert(0, numbers_list)
-
-    file_name, file_ext = os.path.splitext(file)
-    file_new = file_name + "_new" + file_ext
-    with open(file_new, "w") as f:
-        f.write("".join(contents))
-
 def create_random_dataset(max, bmp_dir, filename):
+    header_written = False
     file_count = len(glob.glob(bmp_dir))
     rnd_numbers = np.random.randint(0, file_count, max)
     outfile = filename + "_rnd.csv"
     with open(outfile, mode='w', newline='') as diceroll_file:
         for file_index in rnd_numbers:
             bmp_file = glob.glob(bmp_dir)[file_index]
-            print("writing file '%s' (at index %d) to file '%s'" %(bmp_file, file_index, outfile))
+            #print("writing file '%s' (at index %d) to file '%s'" %(bmp_file, file_index, outfile))
             bmp_image_obj = BmpImage(bmp_file)
+            file_bmp_data = [bmp_image_obj.value] + bmp_image_obj.bmp_data
+            length = len(file_bmp_data)
+            if not header_written:
+                print("writing column headers ...")
+                numbers = [str(x) for x in range(0, length - 1)]
+                numbers.insert(0, "value")
+                header_writer = csv.writer(diceroll_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                header_writer.writerow(numbers)
+                header_written = True
+                print("writing row contents ...")
             dices_writer = csv.writer(diceroll_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            dices_writer.writerow([bmp_image_obj.value] + bmp_image_obj.bmp_data)
-            length = bmp_image_obj.data_size
-    print("including column headers ...")
-    include_column_titles(outfile, length) #will be 120000
-
-bmp_dir = './Images/target_output/*.*'
-debug = True #switch here
-if (debug):
-    filename = 'diceroll_reduced'
-else:
-    filename = 'diceroll'
-
-include_column_titles("test.csv", 6)
-#create_random_dataset(10, bmp_dir, filename)
-#csv: https://realpython.com/python-csv/
-print('done.')
+            dices_writer.writerow(file_bmp_data)
+                
+    print("done. data written to '%s'" %(outfile))
+    return outfile
