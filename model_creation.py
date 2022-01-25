b409e02b133f7d86c36c1cae4562d577a18f1538
Pedro Isaaco <isaaco.pedro@gmail.com>
2022-01-25 21:41:47 +0100

    Nan eliminated

--- model_creation.py
+++ model_creation.py
@@ -1,9 +1,27 @@
 import pandas as pd
+import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from datetime import datetime
 from process_image import create_random_dataset
+
+def check_set(given_set, set_label):
+    print("checking label '%s':" %(set_label))
+    x = np.isnan(given_set)
+    if np.any(np.isnan(given_set)):
+        print(x)
+        #np.argwhere(np.isnan(given_set))
+        file_name = set_label + "_nan.csv"
+        export_set(given_set, file_name)
+    if not np.any(np.isfinite(given_set)):
+        print("found inf")
+        file_name = set_label + "_inf.csv"
+        export_set(given_set, file_name)
+
+def export_set(given_set, file_name):
+    df = pd.DataFrame(given_set)
+    df.to_csv(file_name)
 
 def print_current_time(message):
     now = datetime.now()
@@ -26,7 +44,9 @@ csv_data_file = "diceroll_reduced_rnd.csv"#create_random_dataset(10, file_path, 
 print_current_time("data created.")
 #read data
 print_current_time("loading file '%s'" %(csv_data_file))
-dices_data = pd.read_csv(csv_data_file)
+dices_dataframe = pd.read_csv(csv_data_file)
+#currently we have problems with some bitmap data - nan values occur, which have to be eliminated
+dices_dataframe = dices_dataframe.interpolate()
 print_current_time("files loaded.")
 
 #process data
@@ -34,11 +54,15 @@ max_columns = 100000
 dices_features = []
 for i in range(0, max_columns - 1):
     dices_features += [str(i)]
-X = dices_data[dices_features]
-y = dices_data.value
+X = dices_dataframe[dices_features]
+y = dices_dataframe.value
 print_current_time("csv read.")
 
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+check_set(train_X, "train_X")
+check_set(val_X, "val_X")
+check_set(train_y, "train_y")
+check_set(val_y, "val_y")
 print_current_time("model trained.")
 for max_leaf_nodes in [5, 50, 500, 5000]:
     my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
